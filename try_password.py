6fbf33f1260a766e13284b41e3d4e529e9d19791
Sean Lin <58102391+chlin61@users.noreply.github.com>
2019-11-27 23:58:51 +0800

    add new file

---
+++ try_password.py
@@ -0,0 +1,10 @@
+password = 'a123456'
+count = 3
+while count > 0:
+	count = count - 1
+	pwd = input('請輸入密碼: ')
+	if pwd == password:
+		print('登入成功')
+		break
+	else:
+		print('密碼錯誤! 你還有' , count , '機會')
