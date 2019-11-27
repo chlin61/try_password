5f8893bdbecb261ddbb147bae33f7d36b7e8c480
Sean Lin <58102391+chlin61@users.noreply.github.com>
2019-11-28 00:16:43 +0800

    update count

--- try_password.py
+++ try_password.py
@@ -1,11 +1,11 @@
 password = 'a123456'
 count = 3
 while count > 0:
+	count = count - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
 		break
 	else:
-		count = count - 1
 		if count > 0:
 			print('密碼錯誤! 你還有' , count , '機會')
